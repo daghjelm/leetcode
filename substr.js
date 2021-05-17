c72042cdb17db81f88152ed7784f55ad99cf5108
Dag Hjelm <dag@isolve.se>
2021-05-17 16:38:56 +0200

    inint

---
+++ substr.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let map = {}
+  // for (let i in s) {
+  //   let char = s[i]
+  // }
+};
+
+let str = 'abcdef'
---
+++ substr.py
@@ -0,0 +1,38 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+      l = 0
+      c = 0
+      strings  = [""]
+      index_dict = {}
+
+      for i, char in enumerate(s):  
+        if char in strings[c]:
+          str_l = len(strings[c])
+          if str_l > l:
+            l = str_l
+          c += 1
+          char_index = index_dict[char]
+          missed_str = s[char_index + 1:i]
+          strings.append(missed_str)
+          index_dict[char] = i
+        else:
+          strings[c] += char
+          index_dict[char] = i
+
+      print(strings)
+
+      #if len(strings[i]) > l:
+      #  l = len(strings[i])
+      
+      return l
+
+sol = Solution()
+str1 = "abcabcbb"
+str2 = "bbbbb"
+str3 = "dvdf"
+print(sol.lengthOfLongestSubstring(str1))
+print(sol.lengthOfLongestSubstring(str2))
+print(sol.lengthOfLongestSubstring(str3))
+
+        
+          
